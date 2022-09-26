2344329a57f6baf5478c8023d6347c77e4a40b33
Foxxx48 <Foxxdim@mail.ru>
2022-09-26 22:56:52 +0300

    Add notification with NotificationManager

--- app/src/main/java/com/fox/services/MainActivity.kt
+++ app/src/main/java/com/fox/services/MainActivity.kt
@@ -1,7 +1,12 @@
 package com.fox.services
 
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.NotificationCompat
 import com.fox.services.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,10 +18,67 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         _binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        with(binding) {
+            btnSimpleService.setOnClickListener {
+                startService(MyService.newIntent(this@MainActivity, 25))
+            }
+
+            btnForegroundService.setOnClickListener {
+                showNotification()
+            }
+
+            btnIntentService.setOnClickListener {
+
+            }
+
+            btnJobDispatcher.setOnClickListener {
+
+            }
+
+            btnJobIntentService.setOnClickListener {
+
+            }
+
+            btnAlarmManager.setOnClickListener {
+
+            }
+
+            btnWorkManager.setOnClickListener {
+
+            }
+        }
+    }
+
+    private fun showNotification() {
+        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val notificationChannel = NotificationChannel(
+                CHANNEL_ID,
+                CHANNEL_NAME,
+                NotificationManager.IMPORTANCE_DEFAULT
+            )
+                notificationManager.createNotificationChannel(notificationChannel)
+        } else {
+            TODO("VERSION.SDK_INT < O")
+        }
+        val notification = NotificationCompat.Builder(this, CHANNEL_ID)
+            .setContentTitle("Title")
+            .setContentText("Text")
+            .setSmallIcon(R.drawable.ic_launcher_foreground)
+            .build()
+
+
+        notificationManager.notify(1, notification)
     }
 
     override fun onDestroy() {
         super.onDestroy()
         _binding = null
+    }
+
+    companion object {
+        private const val CHANNEL_ID = "channel_id"
+        private const val CHANNEL_NAME = "channel_name"
+
     }
 }
